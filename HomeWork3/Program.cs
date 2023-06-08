e1fcf51ad95952ebbb83b48d40e2a4c1024a73a5
Danich97 <danilashariy23@gmail.com>
2023-06-08 23:09:11 +0300

    done

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Please input your number: ");
+string x = Console.ReadLine();
+int val = int.Parse(x);
+
+bool isPrime = CheckPrime(val);
+
+if (isPrime)
+{
+    Console.WriteLine($"{val} is a prime number.");
+}
+else
+{
+    Console.WriteLine($"{val} is not a prime number.");
+}
+
+static bool CheckPrime(int val)
+{
+    if (val < 2)
+    {
+        return false;
+    }
+
+    if (val == 2)
+    {
+        return true;
+    }
+
+    int val2 = 2;
+    while (val2 < Math.Sqrt(val))
+    {
+        if (val % val2 == 0)
+        {
+            return false;
+        }
+        val2++;
+    }
+
+    return true;
+}
