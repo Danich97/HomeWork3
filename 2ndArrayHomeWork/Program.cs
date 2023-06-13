79877ef1e789f72fc6b0617a162b963781b7e112
Danich97 <danilashariy23@gmail.com>
2023-06-13 22:34:25 +0300

    2nd HomeWorlk for Arrays

---
+++ 2ndArrayHomeWork/Program.cs
@@ -0,0 +1,17 @@
+﻿const int N = 5;
+int[] array = new int[N];
+
+Console.WriteLine("Input numbers for array: ");
+
+for (int i = 0; i < N; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Reverse array: ");
+
+for (int i = N-1; i >= 0; i--)
+{
+    Console.Write(array[i] + " ");
+}
+Console.ReadLine(); 
