6a14a51198024f49f496d85096300909d59fbbe1
Danich97 <danilashariy23@gmail.com>
2023-06-13 22:15:42 +0300

    new homework

---
+++ 1stArrayHomeWork/Program.cs
@@ -0,0 +1,30 @@
+﻿const int N = 5;
+int[] array = new int[N];
+
+Console.WriteLine("Input numbers for array: ");
+
+for  (int i = 0; i < array.Length; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Input number that you are looking for: ");
+
+int lookingNumber = int.Parse(Console.ReadLine());
+bool matchNumber = true;
+
+for (int i = 0;i < array.Length; i++)
+{
+    if (lookingNumber == array[i])
+    {
+        matchNumber = true;
+        Console.WriteLine("The array has numer " + lookingNumber + " and its position in array " + (i + 1)); // I wrote i+1 to show user exacty place of the looking number
+        break;
+    }
+    else matchNumber = false;
+}
+if (!matchNumber)
+{
+    Console.WriteLine("The array doesn't include number " + lookingNumber);
+}
+
